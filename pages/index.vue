0ee2b736dd39149af11f56429afe4f743428bff1
Kento Shirasawa <shirasawa.kento@gmail.com>
2021-02-23 20:54:46 +0900

    re init

--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,54 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        krado
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+	<main>
+		<h1 style="display: block; margin: 0; cursor: default; text-shadow: 0 0 2px grey; font-family:'Comfortaa', cursive;">Krado</h1>
+		<h2 style="display: block; margin-top: 0; cursor: default; text-shadow: 0 0 3px grey; font-family:'Comfortaa', cursive;">Now Brewing...🍷</h2>
+		<small style="display: inline-block; margin-top: 24px; padding: 4px 8px; border-radius: 1em; background: #fff; border: solid 1px #ccc;"><a href="https://www.novalumo.llc/" target="_blank">&copy; 2020 Novalumo G.K.</a></small>
+	</main>
 </template>
 
 <script>
-export default {}
+export default {
+  layout: '',
+  head() {
+    return {
+      title: 'Krado - The Best CSS Framework Ever.'
+    }
+  }
+}
 </script>
 
 <style lang="scss">
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
+@import url("https://fonts.googleapis.com/css2?family=Comfortaa:wght@400;700&family=Lato:wght@400;700&family=Noto+Sans+JP:wght@400;700&display=swap");
+html, body {
+  margin: 0;
+  padding: 0;
+}
+body {
   display: flex;
+  flex-flow: wrap;
   justify-content: center;
   align-items: center;
+  width: 100%;
+  height: 100vh;
+  background: #f8f9fa;
+  color: #333;
+  font-family:'Lato', 'Noto Sans JP', 'ヒラギノ角ゴ ProN', 'Hiragino Kaku Gothic ProN', 'メイリオ', Meiryo, 'ＭＳ Ｐゴシック', 'MS PGothic', sans-serif;
+  user-select: none;
+}
+main {
   text-align: center;
 }
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+a {
+  text-decoration: none;
+  color: #333;
 }
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
+small:hover {
+  opacity: .8;
 }
-
-.links {
-  padding-top: 15px;
+h1 {
+  font-size: 3.7rem;
+}
+h2 {
+  font-size: 2rem;
 }
 </style>
---
+++ pages/sample.vue
@@ -0,0 +1,16 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  layout: '',
+  head() {
+    title: 'Krado - The Best CSS Framework Ever.';
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
